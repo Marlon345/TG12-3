e9fc1682f32b2e9ea8d0e247724859944fdd8b95
Marlon345 <125478720+Marlon345@users.noreply.github.com>
2025-10-21 09:00:52 +0000

    ss

---
+++ Fussballspiel/model.py
@@ -0,0 +1,19 @@
+
+from pydantic import BaseModel, Field, field_validator, ValidationError
+from typing import ClassVar
+
+class Spieler(BaseModel):
+    anzahl_Spieler : ClassVar[int] = 0
+    name : str = Field(default="None")
+    position : str = Field(default="None")
+    motivation : int = Field(default=5, ge=1, le=10)
+
+    def model_post_init(self, __context):
+        Spieler.anzahl_Spieler += 1
+        print(Spieler.anzahl_Spieler)
+
+    model_config = {"validate_assignment": True}
+    
+
+s = Spieler(name="Jan", position="Sturm", motivation=10)
+print(s.model_dump)
--- Fussballspiel/server.py
+++ Fussballspiel/server.py
@@ -1,16 +1,11 @@
 from flask import Flask, request, jsonify
-
+from model import Spieler
 app = Flask(__name__)
 
 # Route für die Hauptseite
-@app.route('/profil')
+@app.route('/')
 def home():
-    return "Marlon Mattes"
-
-@app.route('/profil/JanLeidgold')
-def gold():
-    return "Hallo"
-
+    return "Server ist bereit und wartet auf Anfragen."
 
 # Route zum Empfangen von Nachrichten
 @app.route('/message', methods=['POST'])
@@ -21,6 +16,14 @@ def handle_message():
     response_message = f"Echo: {message}"
     return jsonify({"response": response_message})
 
+@app.route('/student"', methods=['POST'])
+
+@app.route("/student", methods=["GET"])
+def send_student():
+    s = Spieler(name="Jan", position="Sturm", motivation="10")
+    return jsonify(s.model_dump())
+
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=12345)  # Server starten
-
